5d03a840386538749846f05fd3d0603968b4de89
Mattymatt1 <166906010+Mattymatt1@users.noreply.github.com>
2025-05-27 11:41:02 -0500

    Create PlayerCode.lua

---
+++ PlayerCode.lua
@@ -0,0 +1,40 @@
+-- developing the positions of the person class
+function vec2D(x, y)
+  v = { x = x or 0,
+         y = y or 0}  
+       
+setmetatable(v, addVecs)
+     setmetatable(v, creationtable)
+     return v
+end
+
+
+addVecs = {__add = function(a, b)
+       return vec2D(a.x + b.x, a.y + b.y)
+end, __call = function(self)
+   print(self.x .. ", " .. self.y)    
+end, __sub = function(a, b) 
+       return vec2D(a.x - b.x, a.y - b.y ) end
+}
+-- develop persons class
+function personnew(person, name, posx, posy)
+              local  person = {}
+   person.name = name
+   person.posx = vec2D(x)
+       person.posy  = vec2D(y)
+              return person
+       end
+
+creationtable = {personnew}
+
+
+
+-- test
+local Matthew = personnew(Matthew, "Matthew", 40, 60)
+
+print(Matthew.name)
+
+
+
+
+
